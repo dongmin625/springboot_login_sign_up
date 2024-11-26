630ce36c0e2667750ad769e4e1f28b86b2b3e62d
Dong Min Park <focuspdm4383@naver.com>
2024-11-26 22:05:23 +0900

    Update UserController.java
    Login Complete

--- UserController.java
+++ UserController.java
@@ -1,12 +1,15 @@
 package com.example.pupupprojectserver.domain.User;
 
+import com.example.pupupprojectserver.domain.User.dto.LoginDTO;
 import com.example.pupupprojectserver.domain.User.dto.UserDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -42,6 +45,26 @@ public class UserController {
         List<UserDTO> userDTOs = userService.findAllUsers(); // List<UserDTO> 반환
         return ResponseEntity.ok(userDTOs);
     }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginDTO loginDTO) {
+        boolean isSuccess = userService.login(loginDTO.getUsername(), loginDTO.getPassword());
+
+        Map<String, String> response = new HashMap<>();
+        if (isSuccess) {
+            response.put("message", "로그인 성공");
+            return ResponseEntity.ok(response);
+        } else {
+            response.put("message", "로그인 실패");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        }
+    }
+
+
 }
+
+
+
 
 
